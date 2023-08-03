4ef8e78dbed7795a58ae57d8cb37adf46f7db545
ecekoprucu <ecekoprucu@gmail.com>
2023-08-03 11:03:33 +0200

    basic functionality add

--- server/server.js
+++ server/server.js
@@ -9,11 +9,20 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 
+function b64EncodeUnicode(str) {
+  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+      return String.fromCharCode('0x' + p1);
+  }));
+}
+
+
 app.post('/api/data', async (req, res) => {
-    const { email } = req.body;
-  
+    const { email, password, action, clientId, userId, ticketId, message, subject } = req.body;
+
+    const encodedSubject = subject ? b64EncodeUnicode(subject) : null;
+    console.log(`https://bridge.bigcore.net/includes/api.php?username=${process.env.REACT_APP_API_USERNAME}&password=${process.env.REACT_APP_API_PASSWORD}&responsetype=JSON&action=${action}${(!!email && '&email=') || ''}${!!email && email}${(!!userId && '&userid=') || ''}${(!!userId && userId) || ''}${(!!clientId && '&clientid=') || ''}${(!!clientId && clientId) || ''}${(!!password && '&password2=' || '')}${(!!password && password) || ''}${(!!ticketId && '&ticketid=') || ''}${(!!ticketId && ticketId) || ''}${(!!message && '&message=') || ''}${(!!message && message) || ''}${(!!encodedSubject && '&customfields=') || ''}${(!!encodedSubject && encodedSubject) || ''}`);
     try {
-      const response = await axios.post(`https://bridge.bigcore.net/includes/api.php?username=${process.env.REACT_APP_API_USERNAME}&password=${process.env.REACT_APP_API_PASSWORD}&responsetype=JSON&action=GetClients&search=${email}`);
+      const response = await axios.post(`https://bridge.bigcore.net/includes/api.php?username=${process.env.REACT_APP_API_USERNAME}&password=${process.env.REACT_APP_API_PASSWORD}&responsetype=JSON&action=${action}${(!!email && '&email=') || ''}${!!email && email}${(!!userId && '&userid=') || ''}${(!!userId && userId) || ''}${(!!clientId && '&clientid=') || ''}${(!!clientId && clientId) || ''}${(!!password && '&password2=' || '')}${(!!password && password) || ''}${(!!ticketId && '&ticketid=') || ''}${(!!ticketId && ticketId) || ''}${(!!message && '&message=') || ''}${(!!message && message) || ''}${(!!encodedSubject && '&customfields=') || ''}${(!!encodedSubject && encodedSubject) || ''}`);
 
       const data = response.data;
       console.log(data);
